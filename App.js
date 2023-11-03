a242b575b38512a9ab0bc1f3f297103cd924bb6d
Brendan Cordova <cordovalegacy19@gmail.com>
2023-11-02 20:07:55 -0700

    Got background set, title complete, container done

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+// !Packages
+import { StatusBar } from 'expo-status-bar'
+import { SafeAreaView, Text, View, ImageBackground, Image, FlatList } from 'react-native'
+
+// !Styles
+import { app } from './styles/app'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <>
+      <StatusBar style='dark' />
+      <SafeAreaView style={app.screen}>
+        <ImageBackground style={app.imageBg} source={require('./assets/poke-bg.png')} resizeMode='cover'>
+          <View style={app.header}>
+            <Image style={app.imageTitle} source={require('./assets/title.png')}/>
+            <Text style={app.title}>PC!</Text>
+          </View>
+        </ImageBackground>
+      </SafeAreaView>
+    </>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ styles/app.js
@@ -0,0 +1,42 @@
+
+import { StyleSheet } from 'react-native'
+
+
+export const app = StyleSheet.create({
+    screen: {
+        flex: 1
+    },
+    container: {
+        flex: 1,
+        backgroundColor: '#ef5350',
+        opacity: "60%",
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        elevation: 4,
+        shadowColor: "black",
+        borderRadius: 5,
+        width: "90%"
+    },
+    imageBg: {
+        flex: 1,
+        width: "100%",
+        justifyContent: 'flex-start',
+        alignItems: "center",
+        paddingTop: 50,
+    },
+    header: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-evenly"
+    },
+    imageTitle: {
+        width: 300,
+        height: 100
+    },
+    title: {
+        fontSize: 50,
+        color: "white",
+        fontStyle: "italic",
+        fontWeight: "900"
+    }
+})
