db7ce923546e5c364b569a8b2123da743af3e2d5
Brendan Cordova <cordovalegacy19@gmail.com>
2023-11-05 13:02:12 -0800

    Done

--- App.js
+++ App.js
@@ -3,9 +3,11 @@
 import { Image } from 'react-native'
 import { StatusBar } from 'expo-status-bar'
 import { NavigationContainer } from '@react-navigation/native'
+import { createDrawerNavigator } from '@react-navigation/drawer'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
 // !Components
+import Favorites from './screens/Favorites'
 import AllPokemon from './screens/AllPokemon'
 import OnePokemon from './screens/OnePokemon'
 import AppWrapper from './constants/AppWrapper'
@@ -13,6 +15,24 @@ import PokeProvider from './context/PokeContext'
 
 // !Routing
 const Stack = createNativeStackNavigator()
+const Drawer = createDrawerNavigator()
+
+const DrawerNavigator = () => (
+  <Drawer.Navigator screenOptions={{
+    drawerContentStyle: { backgroundColor: "#01579b" },
+    drawerInactiveTintColor: "white",
+    drawerActiveTintColor: "red"
+  }}>
+    <Drawer.Screen name='All-Pokemon' options={{ title: "All Pokemon", headerTintColor: "white", headerStyle: { backgroundColor: "red" } }}>
+      {(props) => (
+        <AppWrapper>
+          <AllPokemon {...props} />
+        </AppWrapper>
+      )}
+    </Drawer.Screen>
+    <Drawer.Screen name='Favorites' component={Favorites} />
+  </Drawer.Navigator>
+)
 
 export default function App() {
 
@@ -25,20 +45,14 @@ export default function App() {
             headerStyle: { backgroundColor: "#01579b" },
             headerTintColor: "white"
           }}>
-            <Stack.Screen name='All-Pokemon' options={{ title: "All Pokemon" }} >
-              {(props) => (
-                <AppWrapper>
-                  <AllPokemon {...props} />
-                </AppWrapper>
-              )}
-            </Stack.Screen>
+            <Stack.Screen name='Home' component={DrawerNavigator} />
             <Stack.Screen name="One-Pokemon" options={({ route, navigation }) => {
               const selectedPokemon = route.params.selectedPokemon
               return {
                 title: selectedPokemon.charAt(0).toUpperCase() + selectedPokemon.slice(1),
                 headerRight: () => {
-                  return <Image source={require("./assets/star.png")}/>
-                } 
+                  return <Image source={require("./assets/star.png")} />
+                }
               }
             }} >
               {(props) => (
---
+++ screens/Favorites.jsx
@@ -0,0 +1,10 @@
+
+import { Text, View } from "react-native"
+
+export default function Favorites() {
+  return (
+    <View>
+        <Text>This is the favorites page</Text>
+    </View>
+  )
+}
